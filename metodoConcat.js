e7a471944d3cda8198f938ecd30061190aee4400
pw <programadoraweb3.0@gmail.com>
2023-07-20 19:15:15 -0300

    7th commit

---
+++ metodoConcat.js
@@ -0,0 +1,8 @@
+var pessoa = ["Larissa", "Devapi", "Sidarta", "Sofia"];
+var gerente = ["Elza", "Antonio", "Ingrid"];
+var pessoa1 = pessoa.slice(1,3);
+console.log(pessoa);
+console.log(pessoa1);
+//
+var empresa = pessoa.concat(gerente);
+console.log(empresa);
