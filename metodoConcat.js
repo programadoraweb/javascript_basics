85ebcff67ea20d65406fcfd7fd0ac0e9a9b76662
pw <programadoraweb3.0@gmail.com>
2023-07-20 19:16:28 -0300

    7th commit updated

--- metodoConcat.js
+++ metodoConcat.js
@@ -1,3 +1,13 @@
+let titulo = "Dr.";
+let nome = "Joao da Silva";
+let nome_completo = titulo.concat(nome);
+console.log(nome_completo);
+//
+let array1 = [1,2,3];
+let array2 = [4,5,6];
+let array3 = array1.concat(array2);
+console.log(array3);
+//
 var pessoa = ["Larissa", "Devapi", "Sidarta", "Sofia"];
 var gerente = ["Elza", "Antonio", "Ingrid"];
 var pessoa1 = pessoa.slice(1,3);
