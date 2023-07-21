24340132bf7f5aab05158bda6147154eeca4d812
pw <programadoraweb3.0@gmail.com>
2023-07-21 15:25:55 -0300

    updated branch

---
+++ conditionals_if_else.js
@@ -0,0 +1,79 @@
+// ESTRUCTURAS DE CONTROL
+// IF / ELSE
+// sintaxis
+// if (condicion) {
+//     instruccion
+// } else {
+//     instruccion
+// }
+
+// eg:1
+// if(n1>n2){
+//     console.log("n1 es mayor que n2");
+// }else if(n2>n1){
+//     console.log("n2 es mayor que n1");
+// }else{
+//     console.log("Son iguales");
+// }
+
+//eg:2
+// var testNote = prompt("Which was your assessment note?");
+// var testNote = 8;
+// var minimumNote = 6;
+// if(testNote >= minimumNote){console.log("Congrats, your note is " + testNote)}else if(testNote < minimumNote){console.log("Unfortunatelly you did not make it this year, please try it next time" + testNote)} else {console.log("please introduce a valid number.");}
+
+// eg. 3
+// var priceTicked = 1000;
+// var moneyAvailable = prompt("How much have you gotten?");
+// if(moneyAvailable >= priceTicked) {
+//     alert("You can buy the ticked. Go ahead!");
+// }
+// else{
+//     alert("Sorry, you've gotta keep on saving for next time...");
+// }
+// alert("js will read other lines");
+
+//eg 4. 
+// var priceTicked = 1000;
+// var moneyAvailable = prompt("How much have you gotten?");
+// var age = prompt("what s your current age?"); 
+// if(moneyAvailable >= priceTicked && age >=18) {
+//     alert("You can buy the ticket. Go ahead!");
+// }
+// else if (moneyAvailable <= priceTicked || age <=18) {
+//     alert("Sorry, you either don't have enough money or your under 18. Please get out of the line and come back when you get older! ");
+// }
+// else {
+//     alert("Sorry, you've gotta keep on saving for next time...");
+
+// }
+// alert("js will read other lines");
+
+//eg. 5
+//
+// let passwordDB = "pepe123";
+// let input = "pepe1233";
+// let passwordCheck = input == passwordDB;
+// console.log(passwordCheck);
+// if (passwordCheck === true) {
+//     console.log("Login Correcto");
+// }
+//     else {
+//     console.log("Contrasena Incorrecta");
+// }
+
+
+// eg. 6 
+// let score = 70;
+// if (score > 30) {
+//     console.log("Congrats. This is your game! You ve gotta it all!");
+// } 
+// else if (score > 15) {
+//     console.log("You ve been improving, but still need to practice a bit more.") ;
+// }  
+// else {
+//     console.log("You need some help");
+// }
+
+
+
