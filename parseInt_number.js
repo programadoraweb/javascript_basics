0efbb1546fcc4807fe767cd177ce2dbc85d1dcb2
pw <programadoraweb3.0@gmail.com>
2023-07-21 13:14:51 -0300

    update parseInt_number

---
+++ parseInt_number.js
@@ -0,0 +1,14 @@
+var num1 = 2500;
+var num2 = 1500;
+
+suma = parseInt(num1) + parseInt(num2);
+
+console.log("la suma del numero 1 con el numero 2 es:"+ suma);
+//
+
+var camisetas, pantalones, totalArticulos;
+camisetas="5";
+pantalones=5;
+totalArticulos=Number(camisetas)+pantalones;
+//Se utilizo la funcion Number para transformar la string en numero
+alert("Total Articulo: " + totalArticulos);
