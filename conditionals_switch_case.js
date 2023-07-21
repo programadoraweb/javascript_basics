4fe845dacc5d192a325ab339b245145c3c9d239b
pw <programadoraweb3.0@gmail.com>
2023-07-21 15:33:05 -0300

    upload switch_case branch

---
+++ conditionals_switch_case.js
@@ -0,0 +1,38 @@
+// SWITCH: Se utiliza cuando sabemos los posibles valores de comparacion
+// switch (expression) {
+//     case expression:
+//
+//         break;
+//     default:
+//
+// }
+
+//eg.1
+// var color = "green";
+// switch (color) {
+//     case "red":
+//         console.log("Hold on!!! ");
+//         break;
+//     case "yellow":
+//         console.log("watch out!!!");
+//         break;
+//     case "green":
+//         console.log("Go ahead");
+//         break;
+//     default:
+//         console.log("Please select a correct colour");
+
+// }
+
+// eg.2
+// let typeCard = "Debit Card";
+// switch(typeCard){
+//     case "Debit Card": 
+//         console.log("This is a debit card. Go ahead!");
+//         break;
+//     case "Credit Card": 
+//         console.log("This is a credit card. Do yourself a favor and throw it away!");
+//         break;
+//         default: 
+//         console.log("No Card at all");
+// } 
